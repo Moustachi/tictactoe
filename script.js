c533e000cfa0dca8669f3420bc7215caa05e1cf8
Moustachi <durablehotpocket@gmail.com>
2016-06-04 21:32:01 -0700

    added O player, and message now displays player turn

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ $(function() {
 
     function setMessage(msg) {
     	document.getElementById("message").innerText = msg;
-    };
-
-    function nextMove(square) { //this function will change the inner text of a clicked square class with the value of document.turn
-    	square.innerText = document.turn; //question: why isn't this function affecting all classes tagged with "square"?
     }
+
+    function nextMove(square) { //this function will change the inner text of a clicked square class with the value of document.turn then runs switchturn function
+    	square.innerText = document.turn; //question: why isn't this function affecting all classes tagged with "square"?
+    	switchTurn();
+    }
+
+    function switchTurn() {
+    	if (document.turn == "X") {
+    		document.turn = "O";
+    	} else {
+    		document.turn = "X";
+    	}
+    	setMessage("It's " + document.turn +"'s turn.")
+    }// the O player is now playable. The table cells now need to be restricted to allow only one click allowed per cell.
 
    	$(".square").click(function(){ //when a square class is clicked, run nextMove(this)
    		nextMove(this);
